6c5e9611ed8504ae1a6bbfd8b9f2806f51903c78
Achyut Dev <achyut.dev@gmail.com>
2020-03-07 12:06:49 -0500

    Initial commit

---
+++ src/test/java/org/emela/sampleproject/SampleProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package org.emela.sampleproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SampleProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
